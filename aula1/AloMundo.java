8b2a9825d98b3e8d5592d68820feeb7a1db8c9b2
José Gabriel Santos Gomes <josegabrielg2014@gmail.com>
2026-04-09 21:48:11 -0300

    [Aula 1] Primeiro codigo em java

---
+++ aula1/AloMundo.java
@@ -0,0 +1,7 @@
+public class AloMundo
+{
+      public static void main(String[] args)
+      {
+      System.out.println("Alo mundo!");
+      }
+}
---
+++ aula1/teste1.java
@@ -0,0 +1,7 @@
+public class teste1
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Ola" + args[0] + " voce tem " + args[1] + " anos de idade!");
+	}
+}
