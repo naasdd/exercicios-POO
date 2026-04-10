f6fb66f0a262b38a3dac22dc8184927038d8cd66
José Gabriel Santos Gomes <josegabrielg2014@gmail.com>
2026-04-09 21:48:54 -0300

    [Aula 1] Primeiro codigo em java

--- aula1/teste1.java
+++ aula1/teste1.java
@@ -1,6 +1,6 @@
 public class teste1
 {
-	public static void main(String[] args)
+	public static void main(Srings[] agrs)
 	{
 		System.out.println("Ola" + args[0] + " voce tem " + args[1] + " anos de idade!");
 	}
